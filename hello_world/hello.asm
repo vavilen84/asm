804215da439d872aea6202f3892f31669b0b38dd
vavilen84 <vladimir.teplov@gmail.com>
2022-02-12 15:44:08 +0200

    remove redunant comments

--- hello_world/hello.asm
+++ hello_world/hello.asm
@@ -1,16 +1,16 @@
 section	.text
-   global _start    ; необходимо для линкера (ld)
+   global _start
 
-_start:	            ; сообщает линкеру стартовую точку
-   mov	edx,len     ; длина строки
-   mov	ecx,msg     ; строка
-   mov	ebx,1       ; дескриптор файла (stdout)
-   mov	eax,4       ; номер системного вызова (sys_write)
-   int	0x80        ; вызов ядра
+_start:
+   mov	edx,len
+   mov	ecx,msg
+   mov	ebx,1       ; stdout
+   mov	eax,4       ; sys_write
+   int	0x80
 
-   mov	eax,1       ; номер системного вызова (sys_exit)
-   int	0x80        ; вызов ядра
+   mov	eax,1       ; sys_exit
+   int	0x80
 
 section	.data
-msg db 'Hello, world!', 0xa  ; содержимое строки для вывода
-len equ $ - msg              ; длина строки
+msg db 'Hello, world!', 0xa
+len equ $ - msg
